d95e1227ba965c027e2a956edb016c3afa0bb876
Johan Abde <abdejohan@gmail.com>
2023-10-16 17:03:56 +0200

    Use double quotes

--- components/CategoriesScreen/SideMenuOrder.js
+++ components/CategoriesScreen/SideMenuOrder.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { orderExample } from '../../constants';
-import { Image } from 'react-native';
-import { MinusIcon, PlusIcon, XMarkIcon } from 'react-native-heroicons/solid';
-import { Animated } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { orderExample } from "../../constants";
+import { Image } from "react-native";
+import { MinusIcon, PlusIcon, XMarkIcon } from "react-native-heroicons/solid";
+import { Animated } from "react-native";
 
 const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
   const [order, setOrder] = useState(orderExample);
@@ -35,16 +35,16 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
   return (
     <View
       style={{
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#00000020',
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#00000020",
         zIndex: 1,
       }}
     >
       <Animated.View
         style={{
-          height: '100%',
+          height: "100%",
           transform: [{ translateX: translateXAnim }],
           zIndex: 2,
         }}
@@ -105,7 +105,7 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
             <Text>Total</Text>
             <Text>$24.99</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate('Payment')}
+              onPress={() => navigation.navigate("Payment")}
               className="flex flex-row justify-between bg-pink-500 mt-4 py-2 px-4 rounded-xl"
             >
               <Text className="text-white text-center text-lg font-bold">
