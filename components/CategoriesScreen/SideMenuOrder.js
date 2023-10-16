cae3097dcdaac2830faf1a1fcaa39998e2a841d3
Johan Abde <abdejohan@gmail.com>
2023-10-16 17:00:02 +0200

    Fix so that order quantity can be changed

--- components/CategoriesScreen/SideMenuOrder.js
+++ components/CategoriesScreen/SideMenuOrder.js
@@ -1,15 +1,29 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-import { orderExample } from "../../constants";
-import { Image } from "react-native";
-import { MinusIcon, PlusIcon, XMarkIcon } from "react-native-heroicons/solid";
-import { Animated } from "react-native";
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { orderExample } from '../../constants';
+import { Image } from 'react-native';
+import { MinusIcon, PlusIcon, XMarkIcon } from 'react-native-heroicons/solid';
+import { Animated } from 'react-native';
 
 const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
   const [order, setOrder] = useState(orderExample);
   const translateXAnim = new Animated.Value(-100);
+
+  const decrementQuantity = (index) => {
+    const updatedOrder = [...order];
+    if (updatedOrder[index].quantitySelected >= 1) {
+      updatedOrder[index].quantitySelected -= 1;
+      setOrder(updatedOrder);
+    }
+  };
+
+  const incrementQuantity = (index) => {
+    const updatedOrder = [...order];
+    updatedOrder[index].quantitySelected += 1;
+    setOrder(updatedOrder);
+  };
 
   Animated.spring(translateXAnim, {
     toValue: sideOrderOpen ? 0 : -100,
@@ -21,21 +35,24 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
   return (
     <View
       style={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        backgroundColor: "#00000020",
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#00000020',
         zIndex: 1,
-      }}>
+      }}
+    >
       <Animated.View
         style={{
-          height: "100%",
+          height: '100%',
           transform: [{ translateX: translateXAnim }],
           zIndex: 2,
-        }}>
+        }}
+      >
         <View
           id="side-order"
-          className="pr-4 pl-4 pt-20 bg-pink-50 absolute right-0 w-[70%] min-h-[100vh] h-full">
+          className="pr-4 pl-4 pt-20 bg-pink-50 absolute right-0 w-[70%] min-h-[100vh] h-full"
+        >
           <View className="flex flex-row justify-between items-start">
             <View className="flex flex-col">
               <Text className="text-lg font-black">My</Text>
@@ -49,7 +66,8 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
           {order.map((item, index) => (
             <View
               key={`${item}${index}-orderitem`}
-              className="flex flex-col justify-center items-center py-4 border-b border-pink-200">
+              className="flex flex-col justify-center items-center py-4 border-b border-pink-200"
+            >
               <View className="w-12 aspect-square rounded-xl mb-3">
                 <Image
                   className="h-full min-w-full rounded-xl"
@@ -65,14 +83,18 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
               </Text>
               <View className="flex flex-row items-center">
                 <TouchableOpacity
-                  className={`w-4 h-2 m-1 px-4 py-3 bg-gray-100 flex justify-center items-center rounded-xl`}>
+                  onPress={() => decrementQuantity(index)}
+                  className={`w-4 h-2 m-1 px-4 py-3 bg-gray-100 flex justify-center items-center rounded-xl`}
+                >
                   <MinusIcon color="black" size={18} />
                 </TouchableOpacity>
                 <Text className="px-1 text-center font-medium text-xs">
                   {item.quantitySelected}
                 </Text>
                 <TouchableOpacity
-                  className={`w-4 h-2 m-1 px-4 py-3 bg-pink-500 flex justify-center items-center rounded-xl`}>
+                  onPress={() => incrementQuantity(index)}
+                  className={`w-4 h-2 m-1 px-4 py-3 bg-pink-500 flex justify-center items-center rounded-xl`}
+                >
                   <PlusIcon color="white" size={18} />
                 </TouchableOpacity>
               </View>
@@ -83,8 +105,9 @@ const SideMenuOrder = ({ sideOrderOpen, setSideOrderOpen }) => {
             <Text>Total</Text>
             <Text>$24.99</Text>
             <TouchableOpacity
-              onPress={() => navigation.navigate("Payment")}
-              className="flex flex-row justify-between bg-pink-500 mt-4 py-2 px-4 rounded-xl">
+              onPress={() => navigation.navigate('Payment')}
+              className="flex flex-row justify-between bg-pink-500 mt-4 py-2 px-4 rounded-xl"
+            >
               <Text className="text-white text-center text-lg font-bold">
                 Order
               </Text>
